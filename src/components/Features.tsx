5bf840365c871613293478f525c351a24f7b5d14
FlashScript <itzflashscript@gmail.com>
2024-08-11 16:13:07 +0330

    Features Section Tablet view is Done

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -3,23 +3,23 @@ import { featureInfo } from "../constants"
 const Features = () => {
 
     return (
-        <section className={`-mt-48 pt-56 pb-52 | bg-primary-ghost-white | flex justify-center items-center | features`}>
+        <section className={`-mt-48 pt-56 md:pt-[17rem] pb-52 | bg-primary-ghost-white | flex justify-center items-center | features`}>
             <ol>
                 {featureInfo.map((item) => (
                     <li 
                         key={item.id} 
-                        className={`mb-12 |  | flex flex-col | `}
+                        className={`mb-12 md:mx-auto md:w-[79.9%] |  | flex flex-col md:flex-row  | `}
                     >
-                        <span className={`px-4 py-2 mx-auto mb-4 | text-xl text-primary-dark-purple font-fraunces font-semibold | border border-primary-dark-purple rounded-full grid place-content-center | `}>
+                        <span className={`${item.id === 1 ? "px-[1.1rem] py-[0.5rem]" : "px-4 py-2"} mx-auto mb-4 md:my-auto | text-xl text-primary-dark-purple font-fraunces font-semibold | border border-primary-dark-purple rounded-full grid place-content-center | `}>
                             {item.listNumber}
                         </span>
 
-                        <div className={` | text-center | flex flex-col gap-4 | `}>
+                        <div className={`md:pl-8 | text-center md:text-left | flex flex-col gap-4 | `}>
                             <h3 className={` | text-2xl text-primary-dark-purple font-fraunces font-bold |  | `}>
                                 {item.title}
                             </h3>
 
-                            <p className={`w-11/12 mx-auto | text-base text-primary-davys-grey font-medium font-manrope |  | `}>
+                            <p className={`w-11/12 mx-auto md:mx-0 | text-base text-primary-davys-grey font-medium font-manrope |  | `}>
                                 {item.desc}
                             </p>
                         </div>
